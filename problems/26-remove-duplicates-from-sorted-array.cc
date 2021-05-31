b81fc9b6890f696a26ea3a5a65616467faa83c4e
Kazato Sugimoto <uiureo@gmail.com>
2021-05-31 23:29:42 +0900

    improve 26

--- problems/26-remove-duplicates-from-sorted-array.cc
+++ problems/26-remove-duplicates-from-sorted-array.cc
@@ -1,5 +1,5 @@
-// Runtime: 980 ms, faster than 5.10% of C++ online submissions for Remove Duplicates from Sorted Array.
-// Memory Usage: 13.6 MB, less than 66.20% of C++ online submissions for Remove Duplicates from Sorted Array.
+// Runtime: 16 ms, faster than 24.97% of C++ online submissions for Remove Duplicates from Sorted Array.
+// Memory Usage: 13.7 MB, less than 23.66% of C++ online submissions for Remove Duplicates from Sorted Array.
 #include<vector>
 #include<string>
 #include<map>
@@ -9,24 +9,24 @@ using namespace std;
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
+      if (nums.size() == 0) return 0;
+
       int i = 0;
-      while (i + 1 < nums.size()) {
-        if (nums[i] == nums[i + 1]) {
-          for (int j = i + 1; j + 1 < nums.size(); j++) {
-            nums[j] = nums[j + 1];
-          }
-          nums.pop_back();
-        } else {
+      for (int j = i + 1; j < nums.size(); j++) {
+        if (nums[i] < nums[j]) {
+          nums[i+1] = nums[j];
           i++;
         }
       }
-      return nums.size();
+
+      return i + 1;
     }
 };
 
 int main() {
   Solution s = Solution();
   vector<int> v = {1,1,2,3,4};
+    // vector<int> v = {1,1,2,2,2,3,4};
   vector<int> v2 = {1,1,1,1,1};
   cout << s.removeDuplicates(v) << endl;
   cout << s.removeDuplicates(v2) << endl;
