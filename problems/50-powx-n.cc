9e1f0d264d79ed8776d6578bf3412bcc7b5dac26
Kazato Sugimoto <uiureo@gmail.com>
2021-06-06 19:08:23 +0900

    add 50

---
+++ problems/50-powx-n.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) return 1.0;
+        if (n < 0) return 1.0 / myPow(x, -(n + 1)) / x;
+
+        if (n % 2 == 0) {
+            return myPow(x * x, n / 2);
+        } else {
+            return x * myPow(x, n - 1);
+        }
+    }
+};
