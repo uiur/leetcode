f12e845e01f003aee8554288889c491e4e4ab55d
zato <uiureo@gmail.com>
2021-07-02 19:16:25 +0900

    Create 0206-reverse-linked-list.cc

---
+++ problems/0206-reverse-linked-list.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == nullptr) return nullptr;
+        if (head->next == nullptr) return head;
+        
+        ListNode* next = head->next;
+        ListNode* reversed_head = reverseList(next);
+        next->next = head;
+        head->next = nullptr;
+        return reversed_head;
+    }
+};
