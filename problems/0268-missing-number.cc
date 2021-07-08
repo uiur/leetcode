e40ccc6d16891f9fc846f296193d379ce649969d
zato <uiureo@gmail.com>
2021-07-08 13:14:48 +0900

    Update 0268-missing-number.cc
    Runtime: 12 ms, faster than 96.88% of C++ online submissions for Missing Number.
    Memory Usage: 17.9 MB, less than 60.61% of C++ online submissions for Missing Number.

--- problems/0268-missing-number.cc
+++ problems/0268-missing-number.cc
@@ -2,12 +2,13 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
-        unordered_set<int> found;
-        for (int num : nums) {
-            found.insert(num);
+        nums.push_back(0);
+        for (int i = 0; i < n; i++) {
+            int num = nums[i] % (n+1);
+            nums[num] += (n+1);
         }
         for (int i = 0; i <= n; i++) {
-            if (found.find(i) == found.end()) return i;
+            if (nums[i] < (n + 1)) return i;
         }
         return -1;
     }
